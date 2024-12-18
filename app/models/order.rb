e3654720af9131679ff587939e3a6e1f2a97851a
Andrea Cano <andreacanogisbert@gmail.com>
2024-12-18 10:46:17 -0800

    Update Order model with new validations and add a total method

--- app/models/order.rb
+++ app/models/order.rb
@@ -20,8 +20,18 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items, dependent: :destroy
+  has_many :products, through: :order_items
 
-  validates :status, presence: true
-  validates :total, numericality: { greater_than_or_equal_to: 0 }
+  # to ensure that an order cannot be created or saved in the database unless it has at least one associated OrderItem
+  validates :order_items, presence: true
+
+  validates :status, presence: true, inclusion: { in: %w[pending completed canceled] }
+  validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+
+  def total
+    order_items.sum { |item| item.price * item.quantity }
+  end
+
 end
 
