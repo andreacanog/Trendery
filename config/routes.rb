c528de04dbef70d7e922cce633997ebc19149d6c
Andrea Cano <andreacanogisbert@gmail.com>
2025-03-10 10:17:58 -0700

    BEF: Update routes with alpha order

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:create, :show, :update, :destroy]
-      resources :products, only: [:index, :create, :show, :update, :destroy]
-      resources :cart_items, only: [:index, :create, :update, :destroy]
-      resource :session, only: [:create, :destroy]
-      resources :orders, only: [:index, :show, :create, :update, :destroy]
       resource :cart, only: [:show, :destroy]
+      resources :cart_items, only: [:index, :create, :update, :destroy]
       resources :order_items, only: [:create, :show, :update, :destroy]
+      resources :orders, only: [:index, :show, :create, :update, :destroy]
+      resources :products, only: [:index, :create, :show, :update, :destroy]
+      resource :session, only: [:create, :destroy]
+      resources :users, only: [:create, :show, :update, :destroy]
     end
   end
 
