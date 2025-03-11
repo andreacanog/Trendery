1397022a8e2c46f2100850690a312fbca71e1805
Andrea Cano <andreacanogisbert@gmail.com>
2025-03-10 17:12:36 -0700

    RS: Redux Setup pt.1 (REVIEWED)

---
+++ frontend/src/reducers/rootReducer.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+// import sessionReducer from './sessionReducer';
+
+export const rootReducer = combineReducers({
+//   session: sessionReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
