e265388f590a55ed6e90cf4ac294d0ea8883b774
Andrea Cano <andreacanogisbert@gmail.com>
2025-03-12 17:08:42 -0700

    RS: Address Jon's comments pt.1 (REVIEWED)

--- frontend/src/reducers/rootReducer.ts
+++ frontend/src/reducers/rootReducer.ts
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
-// import sessionReducer from './sessionReducer';
+import sessionReducer from './sessionReducer';
+
 
 export const rootReducer = combineReducers({
-//   session: sessionReducer,
+    session: sessionReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
