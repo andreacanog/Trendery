2801ecd218cda0d2243007fb190b9924741ae376
Andrea Cano <andreacanogisbert@gmail.com>
2025-03-20 15:32:59 -0700

    FA: Update configuration fo redux store

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,7 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { rootReducer } from './reducers/rootReducer'; 
+import { createStore, applyMiddleware, AnyAction } from "redux";
+import thunk, { ThunkMiddleware } from "redux-thunk";
+import { rootReducer, RootState } from "./reducers/rootReducer";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk as ThunkMiddleware<RootState, AnyAction>),
+);
 
+export type AppDispatch = typeof store.dispatch;
+export type AppState = ReturnType<typeof rootReducer>;
 export default store;
