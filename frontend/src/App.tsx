10427108bda73a0fcd3650464d0de98918c31f89
Andrea Cano <andreacanogisbert@gmail.com>
2025-03-26 16:17:53 -0700

    FA: Add routing with protected dashboard route based on user session

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,30 @@
 import React from "react";
-import Auth from "./components/Auth/auth";
 import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/Login/login";
+import Signup from "./components/Signup/signup";
+import Dashboard from "./components/Dashboard/dashboard";
+import { useSelector } from "react-redux";
+import { RootState } from "./reducers/rootReducer";
 
 const App: React.FC = () => {
+  const session = useSelector((state: RootState) => state.session);
+
   return (
-    <div>
-      <Auth />
-    </div>
+    <Routes>
+      {/* Public routes */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+      {/* Protected route: */}
+      <Route
+        path="/dashboard"
+        element={session.user ? <Dashboard /> : <Navigate to="/login" />}
+      />
+
+      {/* Default redirect */}
+      <Route path="/" element={<Navigate to="/dashboard" />} />
+    </Routes>
   );
 };
 
