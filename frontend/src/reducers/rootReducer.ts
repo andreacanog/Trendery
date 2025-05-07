2cc4065af9599d3663cc13b4d9152ec674388be9
Andrea Cano Gisbert <107948728+andreacanog@users.noreply.github.com>
2025-05-07 11:18:28 -0700

    Feature/Redux setup (#8)

---
+++ frontend/src/reducers/rootReducer.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import sessionReducer from './sessionReducer';
+
+
+export const rootReducer = combineReducers({
+    session: sessionReducer
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
